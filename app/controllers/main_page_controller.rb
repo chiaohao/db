ef64c584063a89970aa4780d28da15a7a83699d6
chiaohao <ksjoey0096@gmail.com>
2016-01-16 23:41:53 +0800

    session init

--- app/controllers/main_page_controller.rb
+++ app/controllers/main_page_controller.rb
@@ -1,2 +1,11 @@
 class MainPageController < ApplicationController
+	def index
+		@customer = Customer.all
+	end
+	def login
+		@customer = Customer.find(params[:cname, :password])
+		if(@customer!=null)
+
+		end
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
+  get 'session/new'
+
   get 'admin' => 'admin#index'
 
   get 'customer/newcustomer'
@@ -92,6 +96,9 @@ Rails.application.routes.draw do
   resources :order_detail
 
   root 'main_page#index'
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
